0e9f8d5fb6134c3d70010c6a2c52f6feb8be9612
David Alejo <davidalejo@MacBook-Pro-de-David-2.local>
2022-12-05 13:47:18 +0100

    arreglar BBDD e integrar Swager

--- src/main/java/com/davidalejo/app/domain/OrderDetail.java
+++ src/main/java/com/davidalejo/app/domain/OrderDetail.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -35,7 +36,7 @@ public class OrderDetail {
 	@OneToOne
 	private Order order;
 	
-	@OneToOne
+	@ManyToOne
 	private Product product;
 	
 }
