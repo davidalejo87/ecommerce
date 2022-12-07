63046a7aa8a3777742eb007ae5bf559104930bcb
David Alejo <davidalejo@MacBook-Pro-de-David-2.local>
2022-12-07 17:30:55 +0100

    creada funcionalida y vista editar products

--- src/main/java/com/davidalejo/app/controller/impl/ProductControllerImpl.java
+++ src/main/java/com/davidalejo/app/controller/impl/ProductControllerImpl.java
@@ -1,10 +1,13 @@
 package com.davidalejo.app.controller.impl;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -48,5 +51,26 @@ public class ProductControllerImpl{
 		
 		return "redirect:/products";
 	}
+	
+	@GetMapping("edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Product product = new Product();
+		Optional<Product> optionalProduct = productService.get(id);
+		product = optionalProduct.get();
+		
+		LOGGER.info("Producto buscado: {}",product);
+		model.addAttribute("product", product);
+		
+		return "products/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Product product) {
+		productService.update(product);
+		
+		
+		return "redirect:/products";
+	}
+	
 	
 }
