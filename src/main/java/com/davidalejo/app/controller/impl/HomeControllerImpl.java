e5616c0149f1244c11b6af76b7ecb59b5dd8b51c
David Alejo <davidalejo@MacBook-Pro-de-David-2.local>
2022-12-08 15:59:39 +0100

    creacion homeController y directorio user con su home

---
+++ src/main/java/com/davidalejo/app/controller/impl/HomeControllerImpl.java
@@ -0,0 +1,26 @@
+package com.davidalejo.app.controller.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.davidalejo.app.service.ProductService;
+
+@Controller
+@RequestMapping("/")
+public class HomeControllerImpl {
+
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping("")
+	public String home(Model model) {
+		
+		model.addAttribute("products", productService.findAll());
+		
+		return "user/home";
+	}
+}
+
