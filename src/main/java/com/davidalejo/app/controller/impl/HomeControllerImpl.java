d9fa8830aa7b5e2c2f76ed6142342b69e8609b53
David Alejo <davidalejo@MacBook-Pro-de-David-2.local>
2022-12-09 03:06:51 +0100

    mostrar vista del producto

--- src/main/java/com/davidalejo/app/controller/impl/HomeControllerImpl.java
+++ src/main/java/com/davidalejo/app/controller/impl/HomeControllerImpl.java
@@ -1,19 +1,27 @@
 package com.davidalejo.app.controller.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.davidalejo.app.controller.HomeController;
 import com.davidalejo.app.service.ProductService;
 
 @Controller
 @RequestMapping("/")
 public class HomeControllerImpl {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
 
 	@Autowired
 	private ProductService productService;
+	
+	// 
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -21,6 +29,15 @@ public class HomeControllerImpl {
 		model.addAttribute("products", productService.findAll());
 		
 		return "user/home";
+	}
+	
+	// Método para cuando haces click en ver producto, te lleve a la vista del producto
+	
+	@GetMapping("product_view/{id}")
+	public String product_view(@PathVariable Integer id) {
+		log.info("Id product enviado como parámetro {} ",id);
+		
+		return "user/product_view";
 	}
 }
 
